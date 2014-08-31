35ffd1df3022963d130cf395efa20f1376db969d
Andoni <andoni.correas@gmail.com>
2014-08-31 15:16:06 +0200

    snippets and beautifulsoup

--- athlet_stats/web/views.py
+++ athlet_stats/web/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.shortcuts import render_to_response,get_object_or_404
 from django.views.generic import TemplateView,ListView,DetailView
+from bs4 import BeautifulSoup
 
 
 
@@ -14,6 +16,13 @@ class PostList(ListView):
 
 
 class PostView(DetailView):
-	model = Post
-	template_name = "web/post.html"
-	context_object_name = "post"
+    model = Post
+    template_name = "web/post.html"
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        context = super(PostView, self).get_context_data(**kwargs)
+        post = get_object_or_404(Post,pk=self.kwargs['pk'])
+        soup = BeautifulSoup(post.body_eu)
+        print soup.find('img')['src']
+        return context
